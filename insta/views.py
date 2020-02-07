64752e55d75942095dab7c9659c6e731022c0535
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-07 12:41:06 +0300

    added past and today post view function

--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse, Http404
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to Le`Insta')
+def post_today(request):
+    date = dt.date.today()
+    # images = Image.get_images()
+    return render(request, 'all-posts/today-ost.html', {"date": date,"image":image})
 
 
 def convert_dates(dates):
@@ -17,16 +22,18 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def past_days_post(request,past_date):
+def past_post(request,past_date):
+    
+    try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Post for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
+        except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+        if date == dt.date.today():
+            return redirect(post_today)  
+          
     return HttpResponse(html)
