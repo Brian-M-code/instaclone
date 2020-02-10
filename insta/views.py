cd96e50689c49ca3b533058272564acc6f4bb08f
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-10 10:01:44 +0300

    added search html file

--- insta/views.py
+++ insta/views.py
@@ -6,3 +6,21 @@ import datetime as dt
 def index(request):
     return render(request, 'index.html')
 
+def search_user(request):
+    if request.method == "GET":
+        search_term = request.GET.get('search')
+        message = '{}'.format(search_term)
+        # searched_user = User.objects.filter(username=search_term).all()
+        try:
+            searched_user = User.objects.filter(username=search_term)
+            searched_posts = Image.objects.filter(user=searched_user)[::-1]
+        except DoesNotExist:
+            raise Http404()
+            return HttpResponseRedirect('homePage')
+    context={
+        'user':searched_user,
+        'message':message,
+        'posts':searched_posts
+    }
+
+    return render(request, 'searchh.html',context)
