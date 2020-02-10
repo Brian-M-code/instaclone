3269106c1109f535e2346bc9ac0d14e6b286cfb7
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-10 12:37:03 +0300

    added models to the admin

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Image,Profile,Comments
+
+class ProfileAdmin(admin.ModelAdmin):
+    filter_horizontal=("followers","following")
 
 # Register your models here.
+admin.site.register(Profile,admin_class=ProfileAdmin)
+admin.site.register(Image)
+admin.site.register(Comments)
