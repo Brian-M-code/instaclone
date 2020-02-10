ca1e9faefc5ae5501070efc928ee4398aae1db0a
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-10 09:55:55 +0300

    added search functionality

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    avatar = ImageField(manual_crop='', null=True)
+    bio = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user.username
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def get_profile(cls):
+        profile=cls.objects.all()
+
+    @classmethod
+    def single_profile(cls, id):
+        single_profile = cls.objects.filter(id=id)
+        return single_profile
+
