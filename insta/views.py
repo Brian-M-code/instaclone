d324804028e0062c90a64a12474550ee711edba2
Brian-M-Code <mutuma.brian@yahoo.com>
2020-02-10 09:12:46 +0300

    modified settings file

--- insta/views.py
+++ insta/views.py
@@ -6,35 +6,3 @@ import datetime as dt
 def index(request):
     return render(request, 'index.html')
 
-def post_today(request):
-    date = dt.date.today()
-    # images = Image.get_images()
-    return render(request, 'all-posts/today-post.html', {"date": date,"image":image})
-
-
-def convert_dates(dates):
-    
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
-
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
-
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
-
-def past_post(request,past_date):
-    
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-
-        if date == dt.date.today():
-            return redirect(post_today)  
-          
-    return HttpResponse(html)
